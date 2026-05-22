a33d72e99039e342fa5094e03d9d494a904beca2
emartx <emad.armoun@gmail.com>
2026-05-22 06:03:21 +0200

    Connect panel show/hide to menu bar click

--- QlipX/App/AppDelegate.swift
+++ QlipX/App/AppDelegate.swift
@@ -8,11 +8,13 @@
 import AppKit
 import SwiftUI
 
-final class AppDelegate: NSObject, NSApplicationDelegate {
+final class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
     private(set) var panel: NSPanel?
+    private var statusItem: NSStatusItem?
 
     func applicationDidFinishLaunching(_ notification: Notification) {
         panel = makePanel()
+        statusItem = makeStatusItem()
     }
 
     private func makePanel() -> NSPanel {
@@ -28,9 +30,43 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
         panel.isMovableByWindowBackground = true
         panel.isReleasedWhenClosed = false
         panel.hidesOnDeactivate = false
+        panel.delegate = self
         panel.center()
         panel.contentView = NSHostingView(rootView: ContentView())
 
         return panel
+    }
+
+    private func makeStatusItem() -> NSStatusItem {
+        let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+
+        if let button = statusItem.button {
+            button.image = NSImage(systemSymbolName: "paperclip", accessibilityDescription: "QlipX")
+            button.image?.isTemplate = true
+            button.imagePosition = .imageOnly
+            button.action = #selector(togglePanel(_:))
+            button.target = self
+            button.sendAction(on: [.leftMouseUp])
+        }
+
+        return statusItem
+    }
+
+    @objc
+    private func togglePanel(_ sender: Any?) {
+        guard let panel else { return }
+
+        if panel.isVisible {
+            panel.orderOut(sender)
+            return
+        }
+
+        NSApp.activate(ignoringOtherApps: true)
+        panel.makeKeyAndOrderFront(sender)
+    }
+
+    func windowShouldClose(_ sender: NSWindow) -> Bool {
+        sender.orderOut(nil)
+        return false
     }
 }
